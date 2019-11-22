598c763ed2424365fe1b9a0b95a872f0c525216b
Anandhi-11 <58019916+Anandhi-11@users.noreply.github.com>
2019-11-22 10:26:57 +0530

    added int variable

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,9 @@
 package com.k2js.sampleprogram.practice;
 
 class HelloWorld{
+	int i=10;
 	public static void main(String... args){
 		System.out.println("Hello world");
+		System.out.println(i);
 	}
 }
