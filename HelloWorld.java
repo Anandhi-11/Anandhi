6bea86429cdbe304f8833b7bb0212f1d3303c2c3
Anandhi-11 <58019916+Anandhi-11@users.noreply.github.com>
2019-11-22 10:33:45 +0530

    final changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ package com.k2js.sampleprogram.practice;
 class HelloWorld{
 	public static void main(String... args){
 		System.out.println("Hello world");
+		System.out.println("program completed");
 	}
 }
